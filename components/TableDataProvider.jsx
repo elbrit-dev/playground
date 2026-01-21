c8a57773bde06b1e08f6b2979b27858bab122ef3
barani0709 <baranidharan@elbrit.org>
2026-01-21 16:27:32 +0530

    updaetd table and provider

--- components/TableDataProvider.jsx
+++ components/TableDataProvider.jsx
@@ -1122,40 +1122,48 @@ const TableDataProvider = (props) => {
                   >
                     <div className="flex-1 overflow-auto">
                       {drawerData && drawerData.length > 0 ? (
-                        <DataProvider
-                          offlineData={drawerData}
-                          dataSource="offline"
-                          useOrchestrationLayer={true}
-                          isAdminMode={true}
-                          salesTeamColumn={null}
-                          salesTeamValues={[]}
-                          hqColumn={null}
-                          hqValues={[]}
-                          columnTypes={columnTypes || {}}
-                          columnTypesOverride={columnTypes || {}}
-                          enableSort={enableSort ?? true}
-                          enableFilter={enableFilter ?? true}
-                          enableSummation={enableSummation ?? true}
-                          enableGrouping={false}
-                          enableDivideBy1Lakh={enableDivideBy1Lakh ?? false}
-                          textFilterColumns={textFilterColumns || []}
-                          visibleColumns={[]}
-                          redFields={redFields || []}
-                          greenFields={greenFields || []}
-                          outerGroupField={null}
-                          innerGroupField={null}
-                          percentageColumns={percentageColumns || []}
-                          hideDataSourceAndQueryKey={true}
-                        >
-                          <DataTableComponent
+                        <TableProvider value={{
+                          ...consolidatedData,
+                          // Override drawer functions to prevent nested drawers
+                          openDrawerWithData: null,
+                          openDrawerForOuterGroup: null,
+                          openDrawerForInnerGroup: null,
+                        }}>
+                          <DataProvider
+                            offlineData={drawerData}
+                            dataSource="offline"
                             useOrchestrationLayer={true}
-                            rowsPerPageOptions={[5, 10, 25, 50, 100, 200]}
-                            defaultRows={10}
-                            scrollable={false}
-                            enableCellEdit={false}
-                            tableName="sidebar"
-                          />
-                        </DataProvider>
+                            isAdminMode={true}
+                            salesTeamColumn={null}
+                            salesTeamValues={[]}
+                            hqColumn={null}
+                            hqValues={[]}
+                            columnTypes={columnTypes || {}}
+                            columnTypesOverride={columnTypes || {}}
+                            enableSort={enableSort ?? true}
+                            enableFilter={enableFilter ?? true}
+                            enableSummation={enableSummation ?? true}
+                            enableGrouping={enableGrouping ?? true}
+                            enableDivideBy1Lakh={enableDivideBy1Lakh ?? false}
+                            textFilterColumns={textFilterColumns || []}
+                            visibleColumns={[]}
+                            redFields={redFields || []}
+                            greenFields={greenFields || []}
+                            outerGroupField={tab.outerGroup || null}
+                            innerGroupField={tab.innerGroup || null}
+                            percentageColumns={percentageColumns || []}
+                            hideDataSourceAndQueryKey={true}
+                          >
+                            <DataTableComponent
+                              useOrchestrationLayer={true}
+                              rowsPerPageOptions={[5, 10, 25, 50, 100, 200]}
+                              defaultRows={10}
+                              scrollable={false}
+                              enableCellEdit={false}
+                              tableName="sidebar"
+                            />
+                          </DataProvider>
+                        </TableProvider>
                       ) : (
                         <div className="flex flex-col items-center justify-center h-full text-center">
                           <i className="pi pi-inbox text-4xl text-gray-400 mb-4"></i>
